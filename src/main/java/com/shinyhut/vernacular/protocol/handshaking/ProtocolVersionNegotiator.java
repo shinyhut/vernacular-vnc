f7c59dfb22f5f3208a8d766237a8bde18503e951
ivasanpag <ivanj.sanchez90@gmail.com>
2024-02-26 09:43:57 +0100

    Remove ProtocolVersionNegotiator validation.
    Remove ProtocolVersionNegotiator validation. It blocks the connection with proprietary manufacturers with version 4.0 such us Intel AMT

--- src/main/java/com/shinyhut/vernacular/protocol/handshaking/ProtocolVersionNegotiator.java
+++ src/main/java/com/shinyhut/vernacular/protocol/handshaking/ProtocolVersionNegotiator.java
@@ -1,13 +1,9 @@
 package com.shinyhut.vernacular.protocol.handshaking;
 
 import com.shinyhut.vernacular.client.VncSession;
-import com.shinyhut.vernacular.client.exceptions.UnsupportedProtocolVersionException;
-import com.shinyhut.vernacular.client.exceptions.VncException;
 import com.shinyhut.vernacular.protocol.messages.ProtocolVersion;
 
 import java.io.IOException;
-
-import static java.lang.Math.min;
 
 public class ProtocolVersionNegotiator {
 
@@ -15,21 +11,10 @@ public class ProtocolVersionNegotiator {
     private static final int MIN_MINOR_VERSION = 3;
     private static final int MAX_MINOR_VERSION = 8;
 
-    public void negotiate(VncSession session) throws IOException, VncException {
-        ProtocolVersion serverVersion = ProtocolVersion.decode(session.getInputStream());
-
-        if (!serverVersion.atLeast(MAJOR_VERSION, MIN_MINOR_VERSION)) {
-            throw new UnsupportedProtocolVersionException(
-                    serverVersion.getMajor(),
-                    serverVersion.getMinor(),
-                    MAJOR_VERSION,
-                    MIN_MINOR_VERSION
-            );
-        }
-
+    public void negotiate(VncSession session) throws IOException {
         ProtocolVersion clientVersion = new ProtocolVersion(
                 MAJOR_VERSION,
-                min(serverVersion.getMinor(), MAX_MINOR_VERSION)
+                MAX_MINOR_VERSION
         );
 
         session.setProtocolVersion(clientVersion);
